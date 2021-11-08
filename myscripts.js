76b2e7facc80c7d65ae791831203f6f560bc227f
Ahmed <ahmedreda152@gmail.com>
2021-11-08 18:45:20 +0200

    fix styles error and js errors

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,19 @@
-const menuButton = document.querySelector(".navMobile");
-const menu = document.querySelector(".menuContainer");
-const closeButton = document.querySelector(".closeButton");
+const menuButton = document.querySelector('.navMobile');
+const menu = document.querySelector('.menuContainer');
+const closeButton = document.querySelector('.closeButton');
 
-menuButton.addEventListener("click", function () {
-  menuButton.classList.toggle("isActive");
-  menu.classList.toggle("isActive");
-});
+menuButton.addEventListener(
+  'click',
+  (myFunction = () => {
+    menuButton.classList.toggle('isActive');
+    menu.classList.toggle('isActive');
+  })
+);
 
-closeButton.addEventListener("click", function () {
-  menuButton.classList.remove("isActive");
-  menu.classList.remove("isActive");
-});
+closeButton.addEventListener(
+  'click',
+  (myFunction = () => {
+    menuButton.classList.remove('isActive');
+    menu.classList.remove('isActive');
+  })
+);
