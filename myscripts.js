065124cda00681ac08b6b4426d11b1a88b26b0ae
Ahmed <ahmedreda152@gmail.com>
2021-11-08 18:50:41 +0200

    fix eslint errors

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,13 @@
-const menuButton = document.querySelector(".navMobile");
-const menu = document.querySelector(".menuContainer");
-const closeButton = document.querySelector(".closeButton");
+const menuButton = document.querySelector('.navMobile');
+const menu = document.querySelector('.menuContainer');
+const closeButton = document.querySelector('.closeButton');
 
-menuButton.addEventListener("click", () => {
-  menuButton.classList.toggle("isActive");
-  menu.classList.toggle("isActive");
+menuButton.addEventListener('click', () => {
+  menuButton.classList.toggle('isActive');
+  menu.classList.toggle('isActive');
 });
 
-closeButton.addEventListener("click", () => {
-  menuButton.classList.remove("isActive");
-  menu.classList.remove("isActive");
+closeButton.addEventListener('click', () => {
+  menuButton.classList.remove('isActive');
+  menu.classList.remove('isActive');
 });
