57ced8f2e0b64399e9be960501d0d91851e5d8fc
Ahmed <ahmedreda152@gmail.com>
2021-11-09 08:34:23 +0200

    finish all mobile menu features

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,9 @@
 const menuButton = document.querySelector('.navMobile');
 const menu = document.querySelector('.menuContainer');
 const closeButton = document.querySelector('.closeButton');
+const menuLink1 = document.querySelector('.menuLink1');
+const menuLink2 = document.querySelector('.menuLink2');
+const menuLink3 = document.querySelector('.menuLink3');
 
 menuButton.addEventListener('click', () => {
   menuButton.classList.toggle('isActive');
@@ -8,6 +11,21 @@ menuButton.addEventListener('click', () => {
 });
 
 closeButton.addEventListener('click', () => {
+  menuButton.classList.remove('isActive');
+  menu.classList.remove('isActive');
+});
+
+menuLink1.addEventListener('click', () => {
+  menuButton.classList.remove('isActive');
+  menu.classList.remove('isActive');
+});
+
+menuLink2.addEventListener('click', () => {
+  menuButton.classList.remove('isActive');
+  menu.classList.remove('isActive');
+});
+
+menuLink3.addEventListener('click', () => {
   menuButton.classList.remove('isActive');
   menu.classList.remove('isActive');
 });
